0e7916ad4ae99c7a97b4427d3200fd469c0df24f
Brett Weisberg <weisberg.brett@gmail.com>
2018-10-11 17:47:48 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-  binding.pry
     #  session[:name] = params[:name]
       user = session[:name]
   end
