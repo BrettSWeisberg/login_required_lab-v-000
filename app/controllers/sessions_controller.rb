290faca8823558e7e82df7d4cbb034014437430d
Brett Weisberg <weisberg.brett@gmail.com>
2018-10-11 17:50:14 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    binding.pry
     if current_user == nil ||current_user.empty?
       redirect_to '/sessions/new'
     else
